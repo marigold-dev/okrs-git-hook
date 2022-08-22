ecd24d8dd0adf5fd7caeaa37eca7c44a78f6698e
JulesGuesnon <jules.guesnon@hetic.net>
2022-08-22 11:53:18 +0200

    feat: Made a clean install script

--- scripts/install.sh
+++ scripts/install.sh
@@ -4,11 +4,19 @@ echo "Please input the endpoint of okrs: "
 
 read url
 
+curl -LSs https://github.com/marigold-dev/okrs-git-hook/releases/download/0.1.0/commit-msg-macos-0.1.0.tar.gz -o .git/hooks/temp.tar.gz
+
+if [[ "$OSTYPE" == "linux-gnu"* ]]; then
+    tar -xf .git/hooks/temp.tar.gz -C .git/hooks/
+elif [[ "$OSTYPE" == "darwin"* ]]; then
+    tar xf .git/hooks/temp.tar.gz -C .git/hooks/
+fi
+
+rm .git/hooks/temp.tar.gz
+
+chmod +x .git/hooks/commit-msg.exe
+
 curl -Ss https://raw.githubusercontent.com/marigold-dev/okrs-git-hook/master/scripts/commit-msg -o .git/hooks/commit-msg
-
-chmod +x .git .git/hooks/commit-msg
-
-curl -Ss https://raw.githubusercontent.com/marigold-dev/okrs-git-hook/main/scipts/commit-msg.sh -o .git/hooks/commit-msg.exe
 
 echo $url > .git/hooks/.config
 
