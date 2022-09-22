aac5da6b637dae625e692a4df2789e0a8a1c1a6e
JulesGuesnon <jules.guesnon@hetic.net>
2022-09-22 15:40:53 +0200

    Releasing 0.2.1

--- scripts/install.sh
+++ scripts/install.sh
@@ -6,9 +6,9 @@ read url
 
 
 if [[ "$OSTYPE" == "linux-gnu"* ]]; then
-    curl -LSs https://github.com/marigold-dev/okrs-git-hook/releases/download/0.2.0/commit-msg-linux-0.2.0.tar.gz -o .git/hooks/temp.tar.gz
+    curl -LSs https://github.com/marigold-dev/okrs-git-hook/releases/download/0.2.1/commit-msg-linux-0.2.1.tar.gz -o .git/hooks/temp.tar.gz
 elif [[ "$OSTYPE" == "darwin"* ]]; then
-    curl -LSs https://github.com/marigold-dev/okrs-git-hook/releases/download/0.2.0/commit-msg-macos-0.2.0.tar.gz -o .git/hooks/temp.tar.gz
+    curl -LSs https://github.com/marigold-dev/okrs-git-hook/releases/download/0.2.1/commit-msg-macos-0.2.1.tar.gz -o .git/hooks/temp.tar.gz
 fi
 
 if [[ "$OSTYPE" == "linux-gnu"* ]]; then
